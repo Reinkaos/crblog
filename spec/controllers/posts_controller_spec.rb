60cf3b10f67370bdb1a2fe827129e99b00650a75
Reinkaos <rj.matthews23@gmail.com>
2016-04-08 16:16:27 +1200

    Post Tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  before do
+    create_list(:post, 2)
+  end
 
+  describe '#index' do
+    it 'assigns @posts' do
+      get :index
+      expect(assigns(:posts)).to eq Post.all
+    end
+  end
+
+  describe '#show' do
+    before do
+      get :show, id: Post.first
+    end
+
+    it 'assigns the correct @post' do
+      expect(assigns(:post)).to eq Post.first
+    end
+  end
 end
